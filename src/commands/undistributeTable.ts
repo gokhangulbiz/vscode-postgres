74f0a2089b1edf95cfaf4bdb823fea5d16d2bc2d
Gokhan Gulbiz <ggulbiz@gmail.com>
2023-01-17 14:38:58 +0300

    Add undistribute table command

---
+++ src/commands/undistributeTable.ts
@@ -0,0 +1,33 @@
+import BaseCommand from "../common/baseCommand";
+import * as vscode from 'vscode';
+import { TableNode } from "../tree/tableNode";
+import { EditorState } from "../common/editorState";
+import { Database } from "../common/database";
+import { Global } from '../common/global';
+import { SqlQueryManager } from '../queries';
+import { QueryResult } from "pg";
+
+export class undistributeTableCommand extends BaseCommand {
+
+  async run(treeNode: TableNode, count: number = 0, withNames: boolean = false, runOnly: boolean = false) {
+    let connection = await Database.createConnection(treeNode.connection);
+
+    try {
+
+      let res: QueryResult = null;
+
+      res = await connection.query("select undistribute_table('" + treeNode.getQuotedTableName()+ "');");
+      vscode.window.showInformationMessage(treeNode.getQuotedTableName()+ " is now undistributed.");
+      return res;
+    }
+    catch (err) {
+      vscode.window.showErrorMessage(err.message);
+      return err;
+    }
+    finally {
+      await connection.end();
+    }
+
+  }
+
+}
